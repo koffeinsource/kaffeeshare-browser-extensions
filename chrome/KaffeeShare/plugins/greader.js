7f67c376ecfec8d536b795c997d29101b3fc6876
Jens Breitbart <jbreitbart@gmail.com>
2013-01-27 19:51:16 +0100

    Fixed #6 icon is reset in greader if current item is changed.

--- chrome/KaffeeShare/plugins/greader.js
+++ chrome/KaffeeShare/plugins/greader.js
@@ -1,7 +1,27 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 	var url = null;
 	try {
+		// get url to share
 		url = jQuery('#current-entry .entry-title-link').attr('href');
+		
+		// find element to observe
+		var target = document.getElementById('current-entry');
+ 
+		// create an observer instance
+		var observer = new WebKitMutationObserver(function(mutations) {
+			chrome.extension.sendMessage({
+				reset_icon : true
+			}, function(response) {
+				// Do stuff on successful response
+				observer.disconnect();
+			});
+		});
+ 
+		// configuration of the observer:
+		var config = { attributes: true, childList: true, characterData: true, subtree: true }
+		 
+		// pass in the target node, as well as the observer options
+		observer.observe(target, config);
 	} catch (e) {
 	}
 	sendResponse({
