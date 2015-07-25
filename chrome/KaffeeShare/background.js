abbbe95390aec43d731f54dd92e31f40873492b2
Jens Breitbart <jbreitbart@gmail.com>
2015-07-25 13:15:59 +0200

    Updated link to the html view.

--- chrome/KaffeeShare/background.js
+++ chrome/KaffeeShare/background.js
@@ -183,7 +183,7 @@ function iconClick() {
 
         if (!settings.get('double_click_for_share')) {
 			resetIcon();
-			url = getServer() + "/k/show/html/" + settings.get('namespace');
+			url = getServer() + "/k/show/www/" + settings.get('namespace');
 			window.open(url, '_blank');
 			window.focus();
         } else {
